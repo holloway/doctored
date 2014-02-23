25f1e8afbd7701d033e4b77bd05586ef07267a94
Matthew Holloway <matthew@holloway.co.nz>
2014-02-23 19:44:28 +1300

    Retaining cursor position when hitting <enter> in Firefox

--- doctored/js/app-util.js
+++ doctored/js/app-util.js
@@ -386,9 +386,10 @@
                 if(br && br.classList.contains(doctored.CONSTANTS.intentional_linebreak)) continue;
                 block = doctored.util.get_closest_block(br);
                 block_clone = block.cloneNode(true);
-                block.parentNode.insertBefore(block_clone, block.nextSibling);
-                node = br;
+                block.parentNode.insertBefore(block_clone, block);
+                node = $("br", block_clone)[0]; //FIXME this matches intentional inline BRs...should be a loop instead that filters those out (or a better selector that avoids them?)
                 to_delete.push(node);
+                
                 while(node) {
                     if(node.classList && node.classList.contains(doctored.CONSTANTS.block_class)) {
                         node = undefined;
@@ -399,7 +400,7 @@
                         node = node.parentNode;
                     }
                 }
-                node = $("br", block_clone)[0]; //FIXME this matches intentional inline BRs...should be a loop instead that filters those out (or a better selector that avoids them?)
+                node = br;
                 to_delete.push(node);
                 while(node) {
                     if(node.classList && node.classList.contains(doctored.CONSTANTS.block_class)) {
